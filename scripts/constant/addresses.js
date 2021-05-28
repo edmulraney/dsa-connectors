0aba17418962df9a66eee73689b55650c3392f63
Edward Mulraney <edmulraney@gmail.com>
2021-05-28 12:17:41 +0100

    update to latest instadapp connectors contract

--- scripts/constant/addresses.js
+++ scripts/constant/addresses.js
@@ -4,7 +4,7 @@ module.exports = {
     auth: "0xd1aff9f2acf800c876c409100d6f39aea93fc3d9",
   },
   core: {
-    connectorsV2: "0xFE2390DAD597594439f218190fC2De40f9Cf1179",
+    connectorsV2: "0x97b0B3A8bDeFE8cB9563a3c610019Ad10DB8aD11",
     instaIndex: "0x2971AdFa57b20E5a416aE5a708A8655A9c74f723",
   },
 };
